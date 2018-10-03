d75dd580c87b7a7b269acfe28664932fea46f979
Jeff Baker <sinerasis@gmail.com>
2018-10-02 21:59:37 -0400

    add comment explaining where library came from

--- mysqlsetup.py
+++ mysqlsetup.py
@@ -1,9 +1,14 @@
+# depends on mysql-connector library, installed via pip
 import mysql.connector
 
 try:
     db = mysql.connector.connect(user="learner", password="python", host="127.0.0.1", database="python_learning")
     cursor = db.cursor()
+    # creates a characters database
     cursor.execute("CREATE TABLE characters (first_name VARCHAR(20), last_name VARCHAR(20))")
+
+    # insert some rows from our file
+    
     db.close()
 except:
     print("Unable to establish database connection.")
